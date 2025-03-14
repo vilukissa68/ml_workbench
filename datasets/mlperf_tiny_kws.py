d7533567eb961459d982bce460a71cab4b2d62bd
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2025-03-14 17:24:23 +0200

    WIP: Continues implementing the KWS dataset

--- datasets/mlperf_tiny_kws.py
+++ datasets/mlperf_tiny_kws.py
@@ -6,11 +6,14 @@ import os
 import tensorflow as tf
 import tensorflow_datasets as tfds
 from tensorflow.python.platform import gfile
-
-URL = "https://codeload.github.com/eembc/energyrunner/tar.gz/main"
-DOWNLOAD_PATH = "./data/kws.tar.gz"
-EXTRACT_PATH = "./data/mlperf_tiny_kws"
-DATA_PATH = EXTRACT_PATH + "/" + "energyrunner-main/datasets/kws01"
+import numpy as np
+import torchaudio
+from torchaudio.transforms import MFCC
+import torch
+from pathlib import Path
+import glob
+import random
+import wavfile
 
 transform = transforms.Compose(
     [
@@ -20,25 +23,73 @@ transform = transforms.Compose(
     ]
 )
 
+NUM_TRAIN_SAMPLES = -1
+NUM_VAL_SAMPLES = -1
+NUM_TEST_SAMPLES = -1
+NUM_MFCCS = 10
+
+BACKGROUND_FREQUENCY = 0.8
+BACKGROUND_VOLUME = 0.1
+SAMPLE_RATE = 16000
+WINDOW_SIZE_MS = 30.0
+WINDOW_STRIDE_MS = 20.0
+WINDOW_STRIDE_SAMPLES = int(SAMPLE_RATE * WINDOW_STRIDE_MS / 1000)
+WINDOW_SIZE_SAMPLES = int(SAMPLE_RATE * WINDOW_SIZE_MS / 1000)
+CLIP_DURATION_MS = 1000
+DESIRED_SAMPLES = int(SAMPLE_RATE * CLIP_DURATION_MS / 1000)  # 16000
+DCT_COEFFICIENT_COUNT = 10
+LENGTH_MINUS_WINDOW = DESIRED_SAMPLES - WINDOW_SIZE_SAMPLES
+SPECTROGRAM_LENGTH = 1 + int(LENGTH_MINUS_WINDOW / WINDOW_STRIDE_SAMPLES)
+DATASET_PATH = Path("./data")
+BACKGROUND_NOISE_DIR = (
+    DATASET_PATH / "SpeechCommands/speech_commands_v0.02/_background_noise_"
+)
+
 
 class MLPerfTinyKWS(BaseDataset):
-    def __init__(self, data_path=DATA_PATH, batch_size=64, shuffle=True):
+    def __init__(self, data_path=DATASET_PATH, batch_size=64, shuffle=True):
         super().__init__(data_path=data_path, batch_size=batch_size, shuffle=shuffle)
         self.has_labels = True
         self.load_data()
 
     def load_data(self):
-        train, test, val = get_training_data()
-        # untar
-        self.trainset = None
-        self.testset = None
+        print("Loading training data...")
+        # train_dataset = SpeechCommandsDataset_pytorch(split="training", augment=True)
+
+        # train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+        print("Loading test data...")
+        test_dataset = SpeechCommandsDataset_pytorch(split="testing", augment=True)
+
+        test_loader = DataLoader(
+            test_dataset, batch_size=self.batch_size, shuffle=False, drop_last=True
+        )
+
+        # self.trainset = train_dataset
+        self.testset = test_dataset
+        print("Data loaded.")
+        # print("Train set size:", len(self.trainset))
+        print("Test set size:", len(self.testset))
+        print("Input:", next(iter(test_loader))[0])
 
     def get_data_shapes(self):
         return {"input_1": (1, 49, 10, 1)}
 
     def get_labels(self):
-        word_labels = ["Down", "Go", "Left", "No", "Off", "On", "Right",
-               "Stop", "Up", "Yes", "Silence", "Unknown"]
+        word_labels = [
+            "Down",
+            "Go",
+            "Left",
+            "No",
+            "Off",
+            "On",
+            "Right",
+            "Stop",
+            "Up",
+            "Yes",
+            "Silence",
+            "Unknown",
+        ]
         return word_labels
 
     def get_num_classes(self):
@@ -50,111 +101,136 @@ class MLPerfTinyKWS(BaseDataset):
         return example
 
 
-class KWSDataset(Dataset):
-    def __init__(self, csv_file, audio_dir, transform=None):
-        """
-        Args:
-            csv_file (string): Path to the csv file with annotations.
-            audio_dir (string): Directory with all the audio files.
-            transform (callable, optional): Optional transform to be applied on a sample.
-        """
-        self.annotations = pd.read_csv(csv_file)
-        self.audio_dir = audio_dir
-        self.transform = transform
+class SpeechCommandsDataset_pytorch(Dataset):
+    def __init__(self, split="train", augment=True):
+        self.split = split
+        self.augment = augment
+        self.waveforms = []
+        self.labels = []
+        self.cache = {}
+        self.use_cache = False
+        self.load_data_pytorch()
+        if self.augment:
+            self.background_data = self.prepare_background_data_pytorch()
+
+    def load_data_pytorch(self):
+        dataset = torchaudio.datasets.SPEECHCOMMANDS(
+            DATASET_PATH, download=True, subset=self.split
+        )
+        print("Dataset length:", len(dataset))
+
+        for waveform, sample_rate, label, *_ in dataset:
+            if sample_rate != SAMPLE_RATE:
+                print("Resampling...")
+                waveform = torchaudio.transforms.Resample(sample_rate, SAMPLE_RATE)(
+                    waveform
+                )
+            self.waveforms.append(waveform)
+            self.labels.append(label)
+
+    def prepare_background_data_pytorch(self):
+        print("Preparing background data...")
+        background_data = []
+        if not os.path.exists(BACKGROUND_NOISE_DIR):
+            print("No background noise found in " + BACKGROUND_NOISE_DIR)
+            return background_data
+
+        search_path = os.path.join(BACKGROUND_NOISE_DIR, "*.wav")
+        print("Extracting background data...")
+        for wav_path in glob.glob(search_path):
+            # Load the audio file
+            waveform, sample_rate = torchaudio.load(wav_path)
+            # Append the raw audio data to the background_data list
+            background_data.append((waveform.squeeze().numpy(), sample_rate))
+
+        if not background_data:
+            raise Exception("No background wav files were found in " + search_path)
+        return background_data
+
+    def add_background_noise_pytorch(self, waveform):
+        if not self.background_data:
+            print("No background noise found.")
+            return waveform
+
+        print("Adding background noise...")
+        noise, sr = random.choice(self.background_data)
+        print("Noise shape:", noise.shape)
+        noise = torch.tensor(noise, dtype=torch.float32) / 32768.0
+        if sr != SAMPLE_RATE:
+            noise = torchaudio.transforms.Resample(sr, SAMPLE_RATE)(noise)
+
+        print("Noise shape:", noise.shape)
+        noise = noise[: waveform.shape[0]]  # Trim or pad noise
+        noise *= random.uniform(0, 0.1)  # Scale noise
+        return waveform + noise
+
+    def get_preprocess_audio_func_pytorch(self, waveform):
+        print("Preprocessing audio...")
+        waveform = waveform.squeeze(0)
+        print("Waveform shape:", waveform.shape)
+        waveform.float()  # Ensure int16
+        waveform = waveform / waveform.abs().max()  # Normalize
+
+        # Pad to desired length
+        if waveform.shape[-1] < DESIRED_SAMPLES:
+            waveform = torch.nn.functional.pad(
+                waveform, (0, DESIRED_SAMPLES - waveform.shape[-1])
+            )
+        elif waveform.shape[-1] > DESIRED_SAMPLES:
+            waveform = waveform[:DESIRED_SAMPLES]
+        print("Padded waveform shape:", waveform.shape)
+
+        time_shift_padding_placeholder_ = torch.tensor([2, 2], dtype=torch.int32)
+        time_shift_offset_placeholder_ = torch.tensor([2], dtype=torch.int32)
+        waveform = torch.nn.functional.pad(
+            waveform,
+            (
+                time_shift_padding_placeholder_[0].item(),
+                time_shift_padding_placeholder_[1].item(),
+            ),
+        )
+        waveform = waveform[
+            time_shift_offset_placeholder_[0]
+            .item() : time_shift_offset_placeholder_[0]
+            .item()
+            + DESIRED_SAMPLES
+        ]
+        print("Padded waveform shape:", waveform.shape)
+
+        if self.augment:
+            waveform = self.add_background_noise_pytorch(waveform)
+
+        # MFCC
+        mfcc_transform = MFCC(
+            sample_rate=SAMPLE_RATE,
+            n_mfcc=DCT_COEFFICIENT_COUNT,
+            log_mels=True,
+            melkwargs={
+                "n_fft": WINDOW_SIZE_SAMPLES,
+                "hop_length": WINDOW_STRIDE_SAMPLES,
+                "n_mels": 40,
+                "f_min": 20,
+                "f_max": 4000,
+                "center": False,
+            },
+        )
+        mfcc = mfcc_transform(waveform)
+        print("MFCC shape:", mfcc.shape)
+        return torch.reshape(mfcc, (1, SPECTROGRAM_LENGTH, DCT_COEFFICIENT_COUNT, 1))
 
     def __len__(self):
-        return len(self.annotations)
+        return len(self.waveforms)
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
+        waveform = self.waveforms[idx]
+        label = self.labels[idx]
 
-        audio_path = f"{self.audio_dir}/{self.annotations.iloc[idx, 0]}.wav"
-        waveform, sample_rate = torchaudio.load(audio_path)
-        label = self.annotations.iloc[
-            idx, 1
-        ]  # Assuming the label is in the second column
-
-        if self.transform:
-            waveform = self.transform(waveform)
-
-        return waveform, label
-
-def get_training_data(background_frequency=1000, background_volume=10):
-    label_count=12
-
-    # Set path for background noise and load
-    bg_path="data/background"
-    BACKGROUND_NOISE_DIR_NAME='_background_noise_'
-    background_data = prepare_background_data(bg_path,BACKGROUND_NOISE_DIR_NAME)
-
-    # Load waveforms data
-    splits = ['train', 'test', 'validation']
-    (ds_train, ds_test, ds_val), ds_info = tfds.load('speech_commands', split=splits,
-                                                    data_dir="data", with_info=True)
-
-    # Get subsets
-    if Flags.num_train_samples != -1:
-        ds_train = ds_train.take(Flags.num_train_samples)
-    if Flags.num_val_samples != -1:
-        ds_val = ds_val.take(Flags.num_val_samples)
-    if Flags.num_test_samples != -1:
-        ds_test = ds_test.take(Flags.num_test_samples)
-
-    # extract spectral features and add background noise
-    ds_train = ds_train.map(get_preprocess_audio_func(model_settings,is_training=True,
-                                                    background_data=background_data),
-                        num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    ds_test  =  ds_test.map(get_preprocess_audio_func(model_settings,is_training=False,
-                                                    background_data=background_data),
-                        num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    ds_val   =   ds_val.map(get_preprocess_audio_func(model_settings,is_training=False,
-                                                    background_data=background_data),
-                        num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    # change output from a dictionary to a feature,label tuple
-    ds_train = ds_train.map(convert_dataset)
-    ds_test = ds_test.map(convert_dataset)
-    ds_val = ds_val.map(convert_dataset)
-
-    # Now that we've acquired the preprocessed data, either by processing or loading,
-    ds_train = ds_train.batch(Flags.batch_size)
-    ds_test = ds_test.batch(Flags.batch_size)
-    ds_val = ds_val.batch(Flags.batch_size)
-
-    return ds_train, ds_test, ds_val
-
-
-def prepare_background_data(bg_path,BACKGROUND_NOISE_DIR_NAME):
-  """Searches a folder for background noise audio, and loads it into memory.
-  It's expected that the background audio samples will be in a subdirectory
-  named '_background_noise_' inside the 'data_dir' folder, as .wavs that match
-  the sample rate of the training data, but can be much longer in duration.
-  If the '_background_noise_' folder doesn't exist at all, this isn't an
-  error, it's just taken to mean that no background noise augmentation should
-  be used. If the folder does exist, but it's empty, that's treated as an
-  error.
-  Returns:
-    List of raw PCM-encoded audio samples of background noise.
-  Raises:
-    Exception: If files aren't found in the folder.
-  """
-  background_data = []
-  background_dir = os.path.join(bg_path, BACKGROUND_NOISE_DIR_NAME)
-  if not os.path.exists(background_dir):
-    return background_data
-  #with tf.Session(graph=tf.Graph()) as sess:
-  #    wav_filename_placeholder = tf.placeholder(tf.string, [])
-  #    wav_loader = io_ops.read_file(wav_filename_placeholder)
-  #    wav_decoder = contrib_audio.decode_wav(wav_loader, desired_channels=1)
-  search_path = os.path.join(bg_path, BACKGROUND_NOISE_DIR_NAME,'*.wav')
-  #for wav_path in gfile.Glob(search_path):
-  #    wav_data = sess.run(wav_decoder, feed_dict={wav_filename_placeholder: wav_path}).audio.flatten()
-  #    self.background_data.append(wav_data)
-  for wav_path in gfile.Glob(search_path):
-    #audio = tfio.audio.AudioIOTensor(wav_path)
-    raw_audio = tf.io.read_file(wav_path)
-    audio = tf.audio.decode_wav(raw_audio)
-    background_data.append(audio[0])
-  if not background_data:
-    raise Exception('No background wav files were found in ' + search_path)
-  return background_data
+        # Check if MFCC features are cached
+        if str(idx) in self.cache and self.use_cache:
+            mfcc_features = self.cache[str(idx)]
+        else:
+            # Cache MFCC features
+            mfcc_features = self.get_preprocess_audio_func_pytorch(waveform)
+            if self.use_cache:
+                self.cache[str(idx)] = mfcc_features
+        return mfcc_features, label
